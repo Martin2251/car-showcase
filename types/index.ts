ec6545b480fd7d303d889d9c667000b72216a66e
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-10 16:16:07 +0100

    before car details modal

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,9 @@ export interface CustomButtonProps {
     containerStyles?: string,
     handleClick?: MouseEventHandler<HTMLButtonElement>
     btnType?: "button" | "submit"
+    textStyles?: string,
+    rightIcon?: string,
+    isDisabled?:boolean,
 }
 
 
@@ -14,3 +17,18 @@ export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturers: string) => void;
 }
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  }
